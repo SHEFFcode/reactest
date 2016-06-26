53488a82e951c889d953d8eba48c8b5ee7c76b5c
Yuriy Shefer <sheff@sheffmachine.com>
2016-06-25 22:17:26 -0700

    finish setting up test_helper

--- test/components/CommentBoxTest.js
+++ test/components/CommentBoxTest.js
@@ -1,4 +1,4 @@
-import { renderComponent, expect } from '../test_helper.js';
+import { renderComponent, expect } from '../test_helper';
 import CommentBox from '../../src/components/CommentBox';
 
 describe('CommentBox', () => {
